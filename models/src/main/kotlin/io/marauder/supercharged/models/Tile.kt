474afdc9152104b73791d76fcc5da4125420e0bc
Florian Zouhar <mail@flozou.de>
2019-01-27 13:13:34 +0100

    added static method toID

--- models/src/main/kotlin/io/marauder/supercharged/models/Tile.kt
+++ models/src/main/kotlin/io/marauder/supercharged/models/Tile.kt
@@ -2,4 +2,9 @@ package io.marauder.supercharged.models
 
 class Tile(val geojson: GeoJSON, val z: Int, val x: Int, val y: Int) {
     fun toID() = (((1 shl z) * y + x) * 32) + z
+    companion object {
+        @JvmStatic
+        fun toID(z: Int, x: Int, y: Int) = (((1 shl z) * y + x) * 32) + z
+    }
 }
+
