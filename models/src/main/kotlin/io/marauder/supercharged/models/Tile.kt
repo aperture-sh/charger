eabc4e456daa7ccdfe9d522bd65ac9e1c5c8222e
Florian Zouhar <mail@flozou.de>
2019-01-27 13:11:13 +0100

    to ID param removed

--- models/src/main/kotlin/io/marauder/supercharged/models/Tile.kt
+++ models/src/main/kotlin/io/marauder/supercharged/models/Tile.kt
@@ -1,5 +1,5 @@
 package io.marauder.supercharged.models
 
 class Tile(val geojson: GeoJSON, val z: Int, val x: Int, val y: Int) {
-    fun toID(z: Int, x: Int, y: Int) = (((1 shl z) * y + x) * 32) + z
+    fun toID() = (((1 shl z) * y + x) * 32) + z
 }
